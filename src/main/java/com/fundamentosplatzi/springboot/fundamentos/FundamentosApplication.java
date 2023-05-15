a6b4f9d9891f3aa747e4f74f3334ff32f35c95cf
FcoArmando <fco_armandos@hotmail.com>
2023-05-15 11:27:10 -0700

    15/05 cambios

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -2,7 +2,9 @@ package com.fundamentosplatzi.springboot.fundamentos;
 
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,10 +15,14 @@ public class FundamentosApplication implements CommandLineRunner {
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency){
+	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
+		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
@@ -27,5 +33,7 @@ public class FundamentosApplication implements CommandLineRunner {
 		componentDependency.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
+		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail() + " Password: " + userPojo.getPassword());
 	}
 }
---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithPropertiesImplement.java
@@ -0,0 +1,14 @@
+package com.fundamentosplatzi.springboot.fundamentos.bean;
+
+public class MyBeanWithPropertiesImplement implements MyBeanWithProperties{
+    String nombre;
+    String apellido;
+    public MyBeanWithPropertiesImplement(String nombre, String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    @Override
+    public String function() {
+        return nombre + " " + apellido;
+    }
+}
